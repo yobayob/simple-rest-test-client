e3d10f38eca848cd8d5fbaecc912d53017009507
ekonechnyi <yobayob@yandex.ru>
2017-01-18 18:26:32 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='strc',
-    version='0.0.31',
+    version='0.0.32',
     description='Micro framework for testing REST API',
     author='yobayob',
     namespace_packages=['simple_rest_test_client'],
